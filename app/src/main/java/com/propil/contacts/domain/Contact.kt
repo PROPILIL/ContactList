3d1be0579b827f12603cfa7c678f5b054958d79a
PROPILIL <83790466+PROPILIL@users.noreply.github.com>
2022-08-18 21:11:33 +0300

    domain layer v1.1

--- app/src/main/java/com/propil/contacts/domain/Contact.kt
+++ app/src/main/java/com/propil/contacts/domain/Contact.kt
@@ -4,10 +4,13 @@ data class Contact(
     val name: String,
     val surname: String,
     val phoneNumber: String,
+    val sex: Int,
     val id: Int = UNDEFINED_ID
 ) {
 
     companion object {
         const val UNDEFINED_ID = -1
+        const val MALE = 0
+        const val FEMALE = 1
     }
 }
